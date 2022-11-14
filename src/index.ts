e32a2fc1985689ce40bb4b8edb7f071dd2d6bb61
Evan Coulson <evan.m.coulson@gmail.com>
2022-11-14 11:52:21 -0800

    Added mixins for generic classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export { JoinPartialClasses } from './partials/join-partial-classes';
+import { Constructor } from '@the-standard/types';
+import { combinePartials } from './partials/combine-partials';
+import { ConstructorsToInstances } from './types/constructors-to-instances';
+
+export function JoinPartialClasses<T extends Constructor[]>(...partials: T) {
+    return combinePartials<ConstructorsToInstances<T>>(class {}, partials);
+}
+
+export function JoinPartialGenericClasses<G>(...partials: Constructor[]): G {
+    return combinePartials(class {}, partials) as G;
+}
--- src/partials/combine-partials.ts
+++ src/partials/combine-partials.ts
@@ -1,12 +1,7 @@
 import { Constructor } from '@the-standard/types';
 import { BundledConstructor } from '../types/bundled-constructor';
-import { ConstructorsToInstances } from '../types/constructors-to-instances';
 
-export function JoinPartialClasses<T extends Constructor[]>(...partials: T) {
-    return combinePartials<ConstructorsToInstances<T>>(class {}, partials);
-}
-
-function combinePartials<T extends any[]>(
+export function combinePartials<T extends any[]>(
     derivedConstructor: Constructor,
     constructors: Constructor[]
 ) {
